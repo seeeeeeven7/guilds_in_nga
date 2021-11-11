699eb8c05c57f0a4b7d2ca76fe04c92628e179e1
seeeeeeven7 <seeeeeeven7@gmail.com>
2021-11-11 20:15:27 +0000

    update

--- main.py
+++
@@ -1,6 +0,0 @@
-import os
-# os.system('python ./crawler/crawler.py')
-# os.system('python ./crawler/parser.py')
-os.system('cp ./crawler/guilds.md ./')
-os.system('git commit -am "update"')
-os.system('git push')
